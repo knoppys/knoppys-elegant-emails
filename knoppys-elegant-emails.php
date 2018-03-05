1dd46d2640ba9a86e2867fe04bd32d4e079db6dc
Alex Knopp <coda@knoppys.co.uk>
2018-03-05 09:29:25 +0000

    Split datatables up into its own file

--- knoppys-elegant-emails.php
+++ knoppys-elegant-emails.php
@@ -3,7 +3,7 @@
 Plugin Name:       Knoppys Elegant Emails
 Plugin URI:        https://github.com/knoppys/knoppys-elegant-emails.git
 Description:       This plugin allows the user to curate property recommendation emails, save and send them. For help and support, please contact coda@knoppys.co.uk
-Version:           5
+Version:           5.1
 Author:            Knoppys Digital Limited
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -32,9 +32,11 @@ foreach ( glob( dirname( __FILE__ ) . '/email-templates/*.php' ) as $root ) {
 ****************************/
 function elegant_scripts() {    
 
-	wp_register_style( 'core', plugin_dir_url( __FILE__ ) . 'css/core.css', false, '1.0.0' );
-	wp_enqueue_style( 'core' );  
-    wp_enqueue_script( 'core', plugin_dir_url( __FILE__ ) . 'js/core.js', array(), '1.0.0', true );
-    wp_localize_script( 'core', 'siteUrlobject', array('siteUrl' => get_bloginfo('url')));   
+	wp_register_style( 'cssCore', plugin_dir_url( __FILE__ ) . 'css/core.css', false, '1.0.0' );
+	wp_enqueue_style( 'cssCore' );  
+    
+    wp_enqueue_script( 'datatables', plugin_dir_url( __FILE__ ) . 'js/datatables.min.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'jsCore', plugin_dir_url( __FILE__ ) . 'js/core.js', array(), '1.0.0', true );
+    wp_localize_script( 'jsCore', 'siteUrlobject', array('siteUrl' => get_bloginfo('url')));   
 }
 add_action( 'admin_enqueue_scripts', 'elegant_scripts' );
