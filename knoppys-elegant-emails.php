62eed24e64e29131cf8c4f5d18c5f826819c4e0c
Alex Knopp <coda@knoppys.co.uk>
2018-03-22 12:35:26 +0000

    Updates

--- knoppys-elegant-emails.php
+++ knoppys-elegant-emails.php
@@ -3,7 +3,7 @@
 Plugin Name:       Knoppys Elegant Emails
 Plugin URI:        https://github.com/knoppys/knoppys-elegant-emails.git
 Description:       This plugin allows the user to curate property recommendation emails, save and send them. For help and support, please contact coda@knoppys.co.uk
-Version:           7.2
+Version:           7.3
 Author:            Knoppys Digital Limited
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -32,13 +32,13 @@ foreach ( glob( dirname( __FILE__ ) . '/email-templates/*.php' ) as $root ) {
 ****************************/
 function elegant_scripts() {    
 
-	wp_register_style( 'datatablessCss', plugin_dir_url( __FILE__ ) . 'css/datatables.min.css', false, '1.0.0' );
-	wp_enqueue_style( 'datatablessCss' );  
+	//wp_register_style( 'datatablessCss', plugin_dir_url( __FILE__ ) . 'css/datatables.min.css', false, '1.0.0' );
+	//wp_enqueue_style( 'datatablessCss' );  
 
 	wp_register_style( 'cssCore', plugin_dir_url( __FILE__ ) . 'css/core.css', false, '1.0.0' );
 	wp_enqueue_style( 'cssCore' );  
     
-    wp_enqueue_script( 'datatables', plugin_dir_url( __FILE__ ) . 'js/datatables.min.js', array(), '1.0.0', true );
+    //wp_enqueue_script( 'datatables', plugin_dir_url( __FILE__ ) . 'js/datatables.min.js', array(), '1.0.0', true );
     wp_enqueue_script( 'jquery-ui-sortable' );   
     wp_enqueue_script( 'jsCore', plugin_dir_url( __FILE__ ) . 'js/core.js', array(), '1.0.0', true );
     wp_localize_script( 'jsCore', 'siteUrlobject', array('siteUrl' => get_bloginfo('url')));   
